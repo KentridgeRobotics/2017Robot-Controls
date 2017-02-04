9bf8345aea9dd523cd783af3f737bac15ebf1a6d
Chris DuPuis <cpdupuis@gmail.com>
2017-02-03 18:46:17 -0800

    Committing Michael's work on AutonomousDrive and CrossBaseline.

---
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/commands/drive/AutonomousDrive.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team3786.robot.commands.drive;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutonomousDrive extends Command {
+	private double myLeftRotation;
+	private double myRightRotation;
+	// LeftRotation and RightRotation is the # degrees to go forwards (negative is backwards)
+    public AutonomousDrive(double leftRotation, double rightRotation) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	myLeftRotation = leftRotation;
+    	myRightRotation = rightRotation;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
