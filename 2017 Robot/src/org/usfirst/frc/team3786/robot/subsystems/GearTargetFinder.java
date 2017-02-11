e6d9c8e2ed535c197673280013384ffb28ff86d6
Anemnox <anemnox@gmail.com>
2017-02-11 12:02:02 -0800

    GearTargetFinder Update - Removed VisionRunner - Implemented CameraServer - Runs GripPipeline on Seperate Thread

--- 2017 Robot/src/org/usfirst/frc/team3786/robot/subsystems/GearTargetFinder.java
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/subsystems/GearTargetFinder.java
@@ -2,13 +2,22 @@ package org.usfirst.frc.team3786.robot.subsystems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team3786.robot.utility.*;
 import org.usfirst.frc.team3786.robot.vision.GripPipeline;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.vision.VisionRunner;
 
@@ -17,13 +26,12 @@ import edu.wpi.first.wpilibj.vision.VisionRunner;
  Find Distance and Angle of Targets in list. 
  */
 public class GearTargetFinder extends Subsystem {
+	//Temporary Resolution
 	private int IMG_WIDTH = 640;
 	private int IMG_HEIGHT = 480;
-	//Temporary Resolution
-	
-	//Camera
-	private final UsbCamera camera;
-	public VisionRunner<GripPipeline> visionRunner;
+		
+	//Fixed ThreadPool for Running Image through Pipeline
+	ExecutorService executorService = Executors.newFixedThreadPool(1);
 	
 	//Default Command
     @Override
@@ -33,19 +41,45 @@ public class GearTargetFinder extends Subsystem {
 	}
     
     //Constructor
-    public GearTargetFinder(UsbCamera cam) {
-    	camera = cam;
-    	visionRunner = new VisionRunner<GripPipeline>(camera, new GripPipeline(), new VisionListener());
-
+    public GearTargetFinder() {
+    	
     }
     
-    // here. Call these from Commands.
-    public void runVisionRunnerOnce() {
-    	visionRunner.runOnce();
+    //Callable method to run Vision Thread on seperate Thread
+    public Callable<ArrayList<MatOfPoint>> runVisionThread() {	
+    	return new Callable<ArrayList<MatOfPoint>>() {
+			@Override
+			public ArrayList<MatOfPoint> call() throws Exception {
+				// TODO Auto-generated method stub
+				UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		    	camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+		    	GripPipeline grip = new GripPipeline();
+		    	
+		    	CvSink cvSink = CameraServer.getInstance().getVideo();
+		        
+		        Mat source = new Mat();
+		        cvSink.grabFrame(source);
+		        grip.process(source);
+				ArrayList<MatOfPoint> output = grip.convexHullsOutput();
+				return output;
+			}
+    	};
+    	
     }
     
+    
+    
+    //Run image through the Grip Pipeline once. 
+    //
+    public List<TargetPosition> executeVisionCamera() throws Exception{
+    	Future<ArrayList<MatOfPoint>> futureResult = executorService.submit(runVisionThread());
+    	ArrayList<MatOfPoint> result = futureResult.get();
+    	return extractTargetPosition(extractContourReports(result));
+    
+    }
+    
+        
     //Return List of ContourReports
-    //Finish Later
     public List<ContourReport> extractContourReports(ArrayList<MatOfPoint> contourMap) {
     	List<ContourReport> contourReports = new ArrayList<ContourReport>();
     	for(MatOfPoint matOfPoint : contourMap) {
@@ -60,6 +94,7 @@ public class GearTargetFinder extends Subsystem {
     	return contourReports;
     }
     
+    
     //Return List of TargetPotitions
     public List<TargetPosition> extractTargetPosition(List<ContourReport> contourReportsList) {
     	List<TargetPosition> tempList = new ArrayList<TargetPosition>();
@@ -73,22 +108,13 @@ public class GearTargetFinder extends Subsystem {
     	return tempList;
     }
     
+    
+    //Display List of ContourReports onto the Console.
     public void displayContourReports(List<ContourReport> contourReportsList) {
     	for(ContourReport report: contourReportsList) {
     		System.out.println(report);
     	}
     }
-    
-    //Output of the GripPipeline
-    //Called after VisionRunner runs once
-    private class VisionListener implements VisionRunner.Listener<GripPipeline> {
-		@Override
-		public void copyPipelineOutputs(GripPipeline pipeline) {
-			// TODO Auto-generated method stub
-			ArrayList<MatOfPoint> output = pipeline.convexHullsOutput();
-			displayContourReports(extractContourReports(output));
-		}
-    }
-
+   
 }
 
