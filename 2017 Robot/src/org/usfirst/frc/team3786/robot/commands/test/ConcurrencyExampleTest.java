ea85f9ee17b2c7e501fb18248377a13c9e26dca3
Chris DuPuis <cpdupuis@gmail.com>
2017-02-11 10:29:03 -0800

    Added ConcurrencyExampleTest

---
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/commands/test/ConcurrencyExampleTest.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team3786.robot.commands.test;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Test;
+
+public class ConcurrencyExampleTest {
+
+	public String makeAString(int count)
+	{
+		if (count <= 0)
+		{
+			return "";
+		}
+		else
+		{
+			return "Spam " + makeAString(count-1);
+		}
+	}
+	
+	Callable<String> callableToMakeAString(int count)
+	{
+		return new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				return makeAString(count);
+			}
+			
+		};
+	}
+	
+	
+	@Test
+	public void testExecuteBecauseWhy() throws Exception {
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		Future<String> resultFuture = executorService.submit(callableToMakeAString(8));
+		String result = resultFuture.get();
+		assertEquals("Spam Spam Spam Spam Spam Spam Spam Spam ", result);
+		
+	}
+
+}
