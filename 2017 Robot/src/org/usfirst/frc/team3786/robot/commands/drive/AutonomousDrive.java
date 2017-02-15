10082814e0ae6b054622b7ecf1d20dfe195a0b19
Chris DuPuis <cpdupuis@gmail.com>
2017-02-14 20:36:04 -0800

    # WARNING: head commit changed in the meantime
    Move ConcurrencyExampleTest to a separate test directory.

--- 2017 Robot/src/org/usfirst/frc/team3786/robot/commands/drive/AutonomousDrive.java
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/commands/drive/AutonomousDrive.java
@@ -8,16 +8,24 @@ import edu.wpi.first.wpilibj.command.Command;
 public class AutonomousDrive extends Command {
 	private double myLeftRotation;
 	private double myRightRotation;
+	private double leftEncoderTicks;
+	private double rightEncoderTicks;
+	
+	static final double encoderRotationsPerWheelRotation = 17.0 / 3.0;
+	//static final double wheelDegreesFwdAndBackPerRobotDegree = ?;
 	// LeftRotation and RightRotation is the # degrees to go forwards (negative is backwards)
     public AutonomousDrive(double leftRotation, double rightRotation) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	myLeftRotation = leftRotation;
     	myRightRotation = rightRotation;
+    	leftEncoderTicks = leftRotation * encoderRotationsPerWheelRotation;
+    	rightEncoderTicks = rightRotation * encoderRotationsPerWheelRotation;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
