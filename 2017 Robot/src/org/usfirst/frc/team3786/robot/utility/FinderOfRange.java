2eb42f007230d3defb3f865aea317fb63a656f89
Chris DuPuis <cpdupuis@gmail.com>
2017-02-15 20:04:09 -0800

    Let's do algebra RIGHT.

--- 2017 Robot/src/org/usfirst/frc/team3786/robot/utility/FinderOfRange.java
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/utility/FinderOfRange.java
@@ -6,10 +6,14 @@ public class FinderOfRange {
 		double y = voltage;
 		double x;
 		
-		x = (19 * Math.sqrt(5))/ ((45 * Math.sqrt(5)) - (Math.sqrt(10239)) -(950*y));
+		x = (19 * Math.sqrt(5))/ ((45 * Math.sqrt(5)) - (Math.sqrt(10239 - 950.0 * y)));
 		
 		return x;
 			
 		
+	}
+	public static void main(String[] args) {
+		System.err.println("r4v of 2.4= "+rangeForVoltage(2.4));
+		System.err.println("r4v of 0.87="+rangeForVoltage(0.8));
 	}
 }
