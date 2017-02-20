ce33b9ddd017d7bea4a07331e83c818b71fe8539
Chris DuPuis <cpdupuis@gmail.com>
2017-02-20 13:29:37 -0800

    RobotAction now has a toAutonomoutDrive method.

--- 2017 Robot/src/org/usfirst/frc/team3786/robot/autonomous/RobotAction.java
+++ 2017 Robot/src/org/usfirst/frc/team3786/robot/autonomous/RobotAction.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team3786.robot.autonomous;
 
+import org.usfirst.frc.team3786.robot.commands.auto.CrossBaseline;
+import org.usfirst.frc.team3786.robot.commands.drive.AutonomousDrive;
+
 public class RobotAction {
 	double angleToTurnDegrees;
 	double distanceToDriveInches;
@@ -15,5 +18,15 @@ public class RobotAction {
 			return "Drive " + distanceToDriveInches + " inches";
 		}
 		
+	}
+	
+	public AutonomousDrive toAutonomousDrive () {
+		if(angleToTurnDegrees != 0) {
+			double wheelDegrees = angleToTurnDegrees * CrossBaseline.wheelRotationDegreesPerRobotTurnDegree;
+			return new AutonomousDrive (wheelDegrees, -wheelDegrees);
+		} else {
+			double wheelDegrees = distanceToDriveInches * CrossBaseline.wheelDegreesPerInch;
+			return new AutonomousDrive (wheelDegrees, wheelDegrees);
+		}
 	}
 }
